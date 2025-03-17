4f383361065afca2a185304ebfb481e5a997d097
Garett Owens <GarettOwens13@gmail.com>
2025-03-16 19:48:48 -0500

    Scanner and ArrayList

---
+++ ToDoList.java
@@ -0,0 +1,6 @@
+import java.util.*;
+
+public class ToDoList {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
